3bb5b3f0f50920890a06030e38c76dcd91ba4001
Alexander Bessonov <bessonovUSA@gmail.com>
2022-12-12 13:52:43 -0600

    Exception

---
+++ src/main/java/com/cydeo/annotation/DefaultExceptionMessage.java
@@ -0,0 +1,15 @@
+package com.cydeo.annotation;
+
+import javax.lang.model.element.Element;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DefaultExceptionMessage {
+
+    String defaultMessage() default "";
+
+}
